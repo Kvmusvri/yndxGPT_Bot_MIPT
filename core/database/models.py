c25525d400269667efce32a123d95272d199c954
Kvmusvri <callmericardomilos@mail.ru>
2025-04-19 22:58:23 +0300

    init

---
+++ core/database/models.py
@@ -0,0 +1,34 @@
+import os
+
+from sqlalchemy import BigInteger, String, LargeBinary, Boolean, TIMESTAMP, DATETIME, JSON
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
+from sqlalchemy.ext.asyncio import AsyncAttrs, async_sessionmaker, create_async_engine
+from dotenv import  load_dotenv
+from sqlalchemy.sql import text
+
+load_dotenv()
+
+engine = create_async_engine(url=os.getenv('DATABASE_URL'))
+
+async_session = async_sessionmaker(engine)
+
+
+class Base(AsyncAttrs, DeclarativeBase):
+    pass
+
+
+class UserStat(Base):
+    __tablename__ = 'user_stats'
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    user_tg_id = mapped_column(BigInteger)
+    datetime = mapped_column(TIMESTAMP(timezone=True))
+    action = mapped_column(String(50))
+
+
+
+async def async_main():
+    async with engine.begin() as conn:
+        await conn.run_sync(Base.metadata.create_all)
+        await conn.execute(text("CREATE EXTENSION IF NOT EXISTS pg_trgm;"))
+        await conn.commit()
